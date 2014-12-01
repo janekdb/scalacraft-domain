e98fe4f9649b08151ba633264cb2c19b82a007b7
Janek Bogucki <janekdb@gmail.com>
2014-12-01 14:02:04 +0000

    SCDM-4: Add class documentation
    Issue-Ref:4

--- scalacraft-domain-net/src/main/scala/org/scalacraft/domain/net/v1/Port.scala
+++ scalacraft-domain-net/src/main/scala/org/scalacraft/domain/net/v1/Port.scala
@@ -18,7 +18,46 @@ package org.scalacraft.domain.net.v1
 import scala.util.control.Exception.catching
 
 /**
- * `Port`
+ * A `Port` represents an IP port.
+ *
+ * The following constraints hold for instances of this class,
+ *
+ * - `portNumber` is in the range [0, 65535]
+ *
+ * An instance can be created using a suitable overload of the `opt` method.
+ *
+ * {{{
+ *   val portOpt: Option[Port] = Port.opt(1908)
+ *   val portOpt2: Option[Port] = Port.opt("40115")
+ * }}}
+ *
+ * When any class constraint would be violated the result is `None`.
+ *
+ * Pattern matching is supported as the following examples demonstrate,
+ * {{{
+ *   7 match {
+ *     case Port(p) => p
+ *     case _ => None
+ *   }
+ * }}}
+ *
+ * The match target can be a string,
+ * {{{
+ * val s: String = ...
+ *   s match {
+ *     case Port(p) => p
+ *     case _ => None
+ *   }
+ * }}}
+ *
+ * Implicit conversions exists which allow an instance of `Port` to be used when an `Int` or `String` is required.
+ *
+ * {{{
+ *   val port = Port.opt(6006)
+ *   val sa = port map {
+ *     p => new InetSocketAddress(p)
+ *   }
+ * }}}
  */
 case class Port private(portNumber: Int)
 
