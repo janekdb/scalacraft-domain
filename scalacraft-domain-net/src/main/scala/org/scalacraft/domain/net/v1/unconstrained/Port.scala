6da8ec2c6062c4fe571209736b23dcc065d05b5b
Janek Bogucki <janekdb@gmail.com>
2014-12-02 19:49:18 +0000

    SCDM-4: Add unconstrained version of Port
    Issue-Ref:4

---
+++ scalacraft-domain-net/src/main/scala/org/scalacraft/domain/net/v1/unconstrained/Port.scala
@@ -0,0 +1,68 @@
+/*
+   Copyright 2014 Janek Bogucki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package org.scalacraft.domain.net.v1.unconstrained
+
+import scala.util.control.Exception._
+
+import org.scalacraft.domain.net.v1.{Port => ConstrainedPort}
+
+/**
+ * A `Port` represents an IP port.
+ *
+ * This classes does not constrain the value of the port in anyway.
+ *
+ * Pattern matching is supported as the following examples demonstrate,
+ * {{{
+ *   7 match {
+ *     case Port(p) => p
+ *     case _ => None
+ *   }
+ * }}}
+ *
+ * The match target can be a string,
+ * {{{
+ * val s: String = ...
+ *   s match {
+ *     case Port(p) => p
+ *     case _ => None
+ *   }
+ * }}}
+ *
+ * Implicit conversions exists which allow an instance of `Port` to be used when an `Int` or `String` is required.
+ *
+ * {{{
+ *   val port = Port(6006)
+ *   val isa = new InetSocketAddress(p)
+ * }}}
+ */
+case class Port(portNumber: Int)
+
+object Port {
+
+  implicit def `to-Int`(port: Port): Int = port.portNumber
+
+  implicit def `to-String`(port: Port): String = port.portNumber.toString
+
+  implicit def `to-Option[Port]`(port: Port): Option[ConstrainedPort] = ConstrainedPort.opt(port.portNumber)
+
+  private implicit class sx(val s: String) {
+    def optInt = catching(classOf[NumberFormatException]) opt s.toInt
+  }
+
+  def unapply(x: Int): Option[Int] = Some(x)
+
+  def unapply(x: String): Option[Int] = x.optInt
+}
---
+++ scalacraft-domain-net/src/test/scala/org/scalacraft/domain/net/v1/unconstrained/PortSpec.scala
@@ -0,0 +1,80 @@
+/*
+   Copyright 2014 Janek Bogucki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package org.scalacraft.domain.net.v1.unconstrained
+
+import org.scalatest.FlatSpec
+
+import org.scalatest.Matchers
+import org.scalatest.OptionValues._
+
+import org.scalacraft.domain.net.v1.{Port => Other}
+
+/**
+ * Specification for an unconstrained `Port`
+ */
+class PortSpec extends FlatSpec with Matchers {
+
+  /* Greater than 65535 */
+  private val PortNumber = 551140
+
+  private val ValidPortNumber = 4580
+
+  private val InvalidPortNumber = -1
+
+  behavior of "A Port"
+
+  it should "be usable in pattern matching" in {
+    def m(x: Int) = x match {
+      case Port(p) => p
+      case _ => None
+    }
+    m(5) should equal(5)
+  }
+
+  it should "be usable in string pattern matching" in {
+    def m(x: String) = x match {
+      case Port(p) => p
+      case _ => None
+    }
+    m(PortNumber.toString) should equal(PortNumber)
+  }
+
+  /* Implicit Conversions */
+
+  it should "implicitly convert to an int" in {
+    val port = Port(PortNumber)
+    val i: Int = port
+    i should equal(PortNumber)
+  }
+
+  it should "implicitly convert to a string" in {
+    val port = Port(PortNumber)
+    val s: String = port
+    s should equal(PortNumber.toString)
+  }
+
+  it should "implicitly convert to a constrained Port when the port is valid" in {
+    val port = Port(ValidPortNumber)
+    val otherOpt: Option[Other] = port
+    otherOpt.value.portNumber should equal(ValidPortNumber)
+  }
+
+  it should "implicitly convert to None when port is invalid" in {
+    val port = Port(InvalidPortNumber)
+    val otherOpt: Option[Other] = port
+    otherOpt should be(None)
+  }
+}
