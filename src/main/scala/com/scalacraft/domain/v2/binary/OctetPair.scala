62ad88981b8e9f086e2d5abf0e1ee02e1061b0a1
Janek Bogucki <janekdb@gmail.com>
2015-04-15 19:29:18 +0100

    SCDM-31: Add constrained OctetPair
    Ref:31

---
+++ src/main/scala/com/scalacraft/domain/v2/binary/OctetPair.scala
@@ -0,0 +1,158 @@
+/*
+   Copyright 2014 - 2015 Janek Bogucki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package com.scalacraft.domain.v2.binary
+
+import com.scalacraft.domain.v2.internal.Information
+
+/**
+ * An `OctetPair` represents two [[Octet]]s.
+ *
+ * The following constraints hold for instances of this class,
+ *
+ * - `hi` is not null
+ * - `lo` is not null
+ *
+ * An instance can be created using a suitable overload of the `opt` method.
+ *
+ * {{{
+ *   val op1: Option[OctetPair] = OctetPair.opt(34540)  // OctetPair(134, 236) = 34540
+ *   val op2: Option[OctetPair] = OctetPair.opt(0x86ec) // OctetPair(134, 236) = 34540
+ *   val op3: Option[OctetPair] = OctetPair.opt("d") // OctetPair(0, 13)
+ *   val op3: Option[OctetPair] = OctetPair.opt("fe") // OctetPair(0, 254)
+ *   val op5: Option[OctetPair] = OctetPair.opt("012") // Decimal 18
+ *   val op4: Option[OctetPair] = OctetPair.opt("f11d") // Decimal 61725
+ * }}}
+ *
+ * Note that the string variant takes between one to four hex characters.
+ *
+ * When any class constraint is violated the result is `None`.
+ * {{{
+ *   val op1: Option[OctetPair] = OctetPair.opt("NaN") // None
+ *   val op2: Option[OctetPair] = OctetPair.opt(0xf1234) // None
+ * }}}
+ *
+ * === Pattern Matching ===
+ *
+ * Pattern matching is supported as the following examples demonstrate,
+ * {{{
+ *   0x3490 match {
+ *     case OctetPair(hi, lo) => (hi, lo) // (Octet(0x34), Octet(0x90)
+ *     case _ => None
+ *   }
+ * }}}
+ *
+ * The match target can be a string,
+ * {{{
+ * val s: String = "4020"
+ *
+ * s match {
+ *   case OctetPair(hi, lo) => (hi, lo) // (Octet(0x40), Octet(0x20)
+ *   case _ => None
+ * }
+ * }}}
+ *
+ * Invalid octet pairs are not matched,
+ * {{{
+ *   -129 match {
+ *     case OctetPair(hi, log) => (hi, lo)
+ *     case _ => None // None
+ *   }
+ * }}}
+ *
+ * === Implicit Conversions ===
+ *
+ * Implicit conversions are supplied which allow an instance of `OctetPair` to be used when an `Int` or `String` is
+ * required.
+ *
+ * {{{
+ *   val Some(pair) = OctetPair.opt(0x40cc)
+ *   val w: Int = 1 + pair // 0x40cd
+ * }}}
+ *
+ * {{{
+ *   val Some(pair) = OctetPair.opt("f")
+ *   val s: String = pair: String // 000f
+ * }}}
+ *
+ * A conversion to the unconstrained version of this class is also available.
+ * @param hi A valid octet representing the high byte
+ * @param lo A valid octet representing the low byte
+ */
+case class OctetPair private(hi: Octet, lo: Octet)
+
+object OctetPair {
+
+  implicit def `to-Int`(octetPair: OctetPair): Int = octetPair.hi * 256 + octetPair.lo
+
+  /**
+   * @example Given OctetPair(Octet(47), Octet(128)) this will return `2f80`
+   * @param octetPair The instance to extract a value from
+   * @return A string representation of the octet pair as four hex character without any prefix
+   */
+  implicit def `to-String`(octetPair: OctetPair): String =
+    Octet.`to-String`(octetPair.hi) + Octet.`to-String`(octetPair.lo)
+
+  // TODO: Move this to Octet.Zero
+  private val ZeroOctet = Octet.opt(0).get
+
+  def opt(x: Int): Option[OctetPair] = unapply(x) map { case (hi, lo) => OctetPair(hi, lo)}
+
+  def opt(x: String): Option[OctetPair] = unapply(x) map { case (hi, lo) => OctetPair(hi, lo)}
+
+  // TODO: Support this null checking pattern in Information object
+  def opt(a: Octet, b: Octet): Option[OctetPair] =
+    for {
+      _ <- Option(a)
+      _ <- Option(b)
+    } yield OctetPair(a, b)
+
+  def unapply(x: String): Option[(Octet, Octet)] = {
+    val octets = extractOctets(x, Nil)
+    octets match {
+      case Some(lo :: Nil) => Some(ZeroOctet, lo)
+      case Some(hi :: lo :: Nil) => Some(hi, lo)
+      case _ => None
+    }
+  }
+
+  // TODO: How to extract without knowing the range of the octet?
+  def unapply(x: Int): Option[(Octet, Octet)] =
+    for {
+      hi <- Octet.opt(x / 256)
+      lo <- Octet.opt(x % 256)
+    } yield (hi, lo)
+
+  /**
+   * No assumptions are made about the format acceptable to [[Octet]]
+   */
+  private def extractOctets(x: String, accum: List[Octet]): Option[List[Octet]] = {
+    Information.whenSome(x, accum) {
+      longestRightMatch(_) match {
+        case Some((unused, octet)) => extractOctets(unused, octet :: accum)
+        case None => None
+      }
+    }
+  }
+
+  private def longestRightMatch(x: String): Option[(String, Octet)] = {
+    /* For example: (,250d), (2,50d), (25,0d), (250,d) */
+    val spans: Seq[(String, String)] = (0 until x.size).toList.map(x splitAt _)
+    val candidates: Seq[(String, Option[Octet])] = spans.map {
+      case (unused, target) => (unused, Octet.opt(target))
+    }
+    candidates.collectFirst { case (unused, Some(octet)) => (unused, octet)}
+  }
+}
---
+++ src/test/scala/com/scalacraft/domain/v2/binary/OctetPairSpec.scala
@@ -0,0 +1,223 @@
+/*
+   Copyright 2014 - 2015 Janek Bogucki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package com.scalacraft.domain.v2.binary
+
+import com.scalacraft.domain.v2.internal.Reflections
+import org.scalatest.{FlatSpec, Matchers}
+
+import org.scalatest.OptionValues._
+
+/**
+ * Specification for `OctetPair`
+ */
+class OctetPairSpec extends FlatSpec with Matchers {
+
+  private implicit class O(x: Int) {
+    def o: Octet = Octet.opt(x).get
+  }
+
+  private val Octet1 = Octet.opt(0x10).get
+  private val Octet2 = Octet.opt(0x81).get
+
+  behavior of "An OctetPair"
+
+  /* Construction from Octets */
+
+  it should "be constructed from non-null inputs" in {
+    val octetPairOpt: Option[OctetPair] = OctetPair.opt(Octet1, Octet2)
+    octetPairOpt.value should have(
+      'hi(Octet1),
+      'lo(Octet2)
+    )
+  }
+
+  it should "not be constructed when any input is null" in {
+    OctetPair.opt(null, Octet2) should be(None)
+    OctetPair.opt(Octet1, null) should be(None)
+    OctetPair.opt(null, null) should be(None)
+  }
+
+  /* Construction from a string */
+
+  it should "be constructed from a valid octet pair number string" in {
+    OctetPair.opt("5").value should have(
+      'hi(0.o),
+      'lo(5.o)
+    )
+    OctetPair.opt("a5").value should have(
+      'hi(0.o),
+      'lo(0xa5.o)
+    )
+    OctetPair.opt("b75").value should have(
+      'hi(0xb.o),
+      'lo(0x75.o)
+    )
+    OctetPair.opt("f00f").value should have(
+      'hi(0xf0.o),
+      'lo(0xf.o)
+    )
+  }
+
+  it should "not be constructed from a null string" in {
+    OctetPair.opt(null: String) should be(None)
+  }
+
+  it should "not be constructed from an empty string" in {
+    OctetPair.opt("") should be(None)
+    OctetPair.opt(" ") should be(None)
+  }
+
+  it should "not be constructed from an invalid numeric string" in {
+    OctetPair.opt("10.9") should be(None)
+  }
+
+  it should "not be constructed from a non-numeric string" in {
+    OctetPair.opt("-") should be(None)
+    OctetPair.opt("3rd") should be(None)
+    OctetPair.opt("0xff") should be(None)
+    OctetPair.opt("g") should be(None)
+  }
+
+  it should "not be constructed from an out of range valid numeric string" in {
+    /* 0x91235 */
+    OctetPair.opt("91235") should be(None)
+    OctetPair.opt("-1") should be(None)
+    OctetPair.opt("-9") should be(None)
+    OctetPair.opt("-fa") should be(None)
+  }
+
+  /* Construction from an int */
+
+  it should "be constructed from an in range int" in {
+    OctetPair.opt(0x0).value should have(
+      'hi(0.o),
+      'lo(0.o)
+    )
+    OctetPair.opt(0x5).value should have(
+      'hi(0.o),
+      'lo(5.o)
+    )
+    OctetPair.opt(0xa5).value should have(
+      'hi(0.o),
+      'lo(0xa5.o)
+    )
+    OctetPair.opt(0xb75).value should have(
+      'hi(0xb.o),
+      'lo(0x75.o)
+    )
+    OctetPair.opt(0xf00f).value should have(
+      'hi(0xf0.o),
+      'lo(0xf.o)
+    )
+    OctetPair.opt(0xffff).value should have(
+      'hi(0xff.o),
+      'lo(0xff.o)
+    )
+  }
+
+  it should "not be constructed from an out of range int" in {
+    OctetPair.opt(-1) should be(None)
+    OctetPair.opt(256 * 256) should be(None)
+  }
+
+  /* Constructor access */
+
+  it should "not have a public constructor" in {
+    val constructors = Reflections.declaredConstructors[OctetPair]
+    constructors should have size 1
+    val con = constructors.head
+    con.isPrivate should be(true)
+  }
+
+  /* Pattern Matching */
+
+  it should "be usable in integer pattern matching" in {
+    def m(x: Int) = x match {
+      case OctetPair(hi, lo) => (hi.octet, lo.octet)
+      case _ => None
+    }
+    m(5) should equal((0, 5))
+    m(255) should equal((0, 255))
+    m(256) should equal((1, 0))
+    m(-1) should be(None)
+    m(0xffff) should equal((255, 255))
+    m(0x10000) should be(None)
+  }
+
+  private val ZeroOctet = Octet.opt(0).get
+
+  it should "be usable in string pattern matching" in {
+    def m(x: String) = x match {
+      case OctetPair(a, b) => (a, b)
+      case _ => None
+    }
+    m(null) should be(None)
+    m("") should be(None)
+    m(" " * 2) should be(None)
+    m("0") should equal((ZeroOctet, ZeroOctet))
+    m("0d") should equal((ZeroOctet, 0xd.o))
+    m("00d") should equal((ZeroOctet, 0xd.o))
+    m("50d") should equal((0x5.o, 0xd.o))
+    m("050d") should equal((0x5.o, 13.o))
+    m("250d") should equal((0x25.o, 13.o))
+    m("0102") should equal((1.o, 2.o))
+    m("f") should equal((ZeroOctet, 15.o))
+    m("e1") should equal((ZeroOctet, 0xe1.o))
+    m("eh") should be(None)
+    m("161") should equal((1.o, 0x61.o))
+    m("12233") should be(None)
+    m("p7788") should be(None)
+    m("d") should equal((ZeroOctet, 0xd.o))
+    m("1d") should equal((ZeroOctet, 0x1d.o))
+    m("51d") should equal((0x5.o, 0x1d.o))
+    m("251d") should equal((0x25.o, 0x1d.o))
+    m("4251d") should be(None)
+  }
+
+  /* Implicit Conversions */
+
+  it should "implicitly convert to an int" in {
+    val Some(octetPair) = OctetPair.opt(0x12fe)
+    val s: Int = octetPair
+    s should equal(0x12fe)
+  }
+
+  it should "implicitly convert to an int with addition" in {
+    val Some(pair) = OctetPair.opt(0x40cc)
+    val w: Int = 1 + pair
+    w should equal(0x40cd)
+  }
+
+  it should "implicitly convert to a string from a valid int" in {
+    val Some(octetPair) = OctetPair.opt(0x12fe)
+    val s: String = octetPair
+    s should equal("12fe")
+  }
+
+  it should "implicitly convert to a string from a valid low int" in {
+    val Some(octetPair) = OctetPair.opt(0xf)
+    val s: String = octetPair
+    s should equal("000f")
+  }
+
+  //  it should "implicitly convert to an unconstrained Octet" in {
+  //    val Some(octet) = Octet.opt(ValidOctet.Number)
+  //    val uncons: unconstrained.Octet = octet
+  //    val a = uncons.octet
+  //    val b = uncons.octet.value
+  //    uncons.octet.value should equal(ValidOctet.Number)
+  //  }
+}
